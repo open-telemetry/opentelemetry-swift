4cbcb42c9a5181a44edff56e053dba36e13d20f1
Ignacio Bonafonte <47814950+nacho-undefinedlabs@users.noreply.github.com>
2020-02-25 08:47:52 +0100

    Update Sources/Exporters/Stdout/StdoutExporter.swift
    Co-Authored-By: Sergey Kanzhelev <S.Kanzhelev@live.com>

--- Sources/Exporters/Stdout/StdoutExporter.swift
+++ Sources/Exporters/Stdout/StdoutExporter.swift
@@ -31,7 +31,7 @@ public class StdoutExporter: SpanExporter {
             print("TraceState: \(span.traceState)")
             print("ParentSpanId: \(span.parentSpanId?.hexString ?? "no Parent")")
             print("Start: \(span.startEpochNanos)")
-            print("Duration: \(span.endEpochNanos - span.startEpochNanos)")
+            print("Duration: \(span.endEpochNanos - span.startEpochNanos) nanoseconds")
             print("Attributes: \(span.attributes)")
             print("------------------\n")
         }
