65237d30a127e13c6ff9cb942d5a103fbed5c1d5
Ignacio Bonafonte <nacho@undefinedlabs.com>
2020-06-02 12:51:44 +0200

    Reset packge changes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
         .executable(name: "loggingTracer", targets: ["LoggingTracer"]),
     ],
     dependencies: [
-        .package(name:"Thrift", url:  "https://github.com/undefinedlabs/Thrift-Swift", from: "1.1.1") // Need custom fork because of SPM
+        .package(url:  "https://github.com/undefinedlabs/Thrift-Swift", from: "1.1.1") // Need custom fork because of SPM
     ],
     targets: [
         .target(  name: "OpenTelemetryApi", dependencies: []),
