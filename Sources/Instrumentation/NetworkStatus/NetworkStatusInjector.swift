8d5c68177021d18f3f8f4a7a2a325e36e3b22888
Bryce Buchanan <bryce.buchanan@elastic.co>
2021-07-19 10:06:15 -0700

    swiftformat

--- Sources/Instrumentation/NetworkStatus/NetworkStatusInjector.swift
+++ Sources/Instrumentation/NetworkStatus/NetworkStatusInjector.swift
@@ -4,43 +4,42 @@
  */
 
 #if os(iOS)
-import Foundation
-import OpenTelemetryApi
-import CoreTelephony
-import Network
-public class NetworkStatusInjector {
-    private var netstat : NetworkStatus
-    
-    public init(netstat: NetworkStatus) {
-        self.netstat = netstat
-    }
-    
-    public func inject(span: Span) {
-        let (type, subtype, carrier) = netstat.status()
-        span.setAttribute(key: "net.host.connection.type", value:AttributeValue.string(type))
+    import CoreTelephony
+    import Foundation
+    import Network
+    import OpenTelemetryApi
+    public class NetworkStatusInjector {
+        private var netstat: NetworkStatus
 
-        if let subtype : String  = subtype {
-            span.setAttribute(key: "net.host.connection.subtype", value: AttributeValue.string(subtype))
+        public init(netstat: NetworkStatus) {
+            self.netstat = netstat
         }
 
-        if let carrierInfo : CTCarrier = carrier {
+        public func inject(span: Span) {
+            let (type, subtype, carrier) = netstat.status()
+            span.setAttribute(key: "net.host.connection.type", value: AttributeValue.string(type))
 
-            if let carrierName = carrierInfo.carrierName {
-                span.setAttribute(key: "net.host.carrier.name", value: AttributeValue.string(carrierName))
+            if let subtype: String = subtype {
+                span.setAttribute(key: "net.host.connection.subtype", value: AttributeValue.string(subtype))
             }
-            
-            if let isoCountryCode = carrierInfo.isoCountryCode {
-                span.setAttribute(key: "net.host.carrier.icc", value: AttributeValue.string(isoCountryCode))
-            }
-            
-            if let mobileCountryCode = carrierInfo.mobileCountryCode {
-                span.setAttribute(key: "net.host.carrier.mcc", value: AttributeValue.string(mobileCountryCode))
-            }
-            
-            if let mobileNetworkCode = carrierInfo.mobileNetworkCode {
-                span.setAttribute(key: "net.host.carrier.mnc", value: AttributeValue.string(mobileNetworkCode))
+
+            if let carrierInfo: CTCarrier = carrier {
+                if let carrierName = carrierInfo.carrierName {
+                    span.setAttribute(key: "net.host.carrier.name", value: AttributeValue.string(carrierName))
+                }
+
+                if let isoCountryCode = carrierInfo.isoCountryCode {
+                    span.setAttribute(key: "net.host.carrier.icc", value: AttributeValue.string(isoCountryCode))
+                }
+
+                if let mobileCountryCode = carrierInfo.mobileCountryCode {
+                    span.setAttribute(key: "net.host.carrier.mcc", value: AttributeValue.string(mobileCountryCode))
+                }
+
+                if let mobileNetworkCode = carrierInfo.mobileNetworkCode {
+                    span.setAttribute(key: "net.host.carrier.mnc", value: AttributeValue.string(mobileNetworkCode))
+                }
             }
         }
     }
-}
 #endif
