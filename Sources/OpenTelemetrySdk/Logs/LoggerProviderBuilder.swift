390c5456fbd2c1c49a72b13285a3826c0d20f5ae
Bryce Buchanan <bryce.buchanan@elastic.co>
2022-11-16 12:49:59 -0800

    fixed processor assignment

--- Sources/OpenTelemetrySdk/Logs/LoggerProviderBuilder.swift
+++ Sources/OpenTelemetrySdk/Logs/LoggerProviderBuilder.swift
@@ -30,7 +30,7 @@ public class LoggerProviderBuilder {
     }
     
     public func with(processors: [LogRecordProcessor]) -> Self {
-        self.logProcessors = processors
+        logProcessors = processors
         return self
     }
     
