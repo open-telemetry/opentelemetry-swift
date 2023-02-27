341b2d111594ee0b0dc01063326654f3fcdb80f4
Kevin Earls <kevinearls@mac.com>
2023-02-27 14:51:49 +0100

    Added HTTP Log exporter and test
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

---
+++ Sources/Exporters/OpenTelemetryProtocol/logs/OtlpHttpLogExporter.swift
@@ -0,0 +1,64 @@
+//
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+// 
+
+import Foundation
+import Logging
+import NIO
+import NIOHPACK
+import OpenTelemetryApi
+import OpenTelemetrySdk
+
+public func defaultOltpHttpLoggingEndpoint() -> URL {
+    URL(string: "http://localhost:4318/v1/logs")!
+}
+
+public class OtlpHttpLogExporter : LogRecordExporter {
+    let endpoint: URL
+    private let httpClient: HTTPClient
+    var pendingLogRecords: [ReadableLogRecord] = []
+    
+    public init(endpoint: URL = defaultOltpHttpLoggingEndpoint()) {
+        self.endpoint = endpoint
+        self.httpClient = HTTPClient()
+    }
+    
+    public func export(logRecords: [OpenTelemetrySdk.ReadableLogRecord]) -> OpenTelemetrySdk.ExportResult {
+        pendingLogRecords.append(contentsOf: logRecords)
+        return self.flush()
+    }
+    
+    public func shutdown() {
+    }
+    
+    public func forceFlush() -> OpenTelemetrySdk.ExportResult {
+        self.flush()
+    }
+    
+    public func flush() -> ExportResult {
+        var exporterResult: ExportResult = .success
+        let logRecords = pendingLogRecords
+        pendingLogRecords = []
+            
+        let body = Opentelemetry_Proto_Collector_Logs_V1_ExportLogsServiceRequest.with { request in
+            request.resourceLogs = LogRecordAdapter.toProtoResourceRecordLog(logRecordList: logRecords)
+        }
+        
+        var request = URLRequest(url: endpoint)
+        request.httpMethod = "POST"
+        request.httpBody = try? body.serializedData()
+        request.setValue("application/x-protobuf", forHTTPHeaderField: "Content-Type")
+            
+        httpClient.send(request: request) { result in
+            switch result {
+            case .success(_):
+                exporterResult = ExportResult.success
+            case .failure(let error):
+                print(error)
+                exporterResult = ExportResult.failure
+            }
+        }
+        return exporterResult
+    }
+}
---
+++ Tests/ExportersTests/OpenTelemetryProtocol/OtlpHttpLogRecordExporterTests.swift
@@ -0,0 +1,67 @@
+//
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+// 
+
+import Foundation
+import Logging
+import NIO
+import NIOHTTP1
+import NIOTestUtils
+import OpenTelemetryApi
+@testable import OpenTelemetryProtocolExporter
+@testable import OpenTelemetrySdk
+import XCTest
+
+class OtlpHttpLogRecordExporterTests: XCTestCase {
+    var testServer: NIOHTTP1TestServer!
+    var group: MultiThreadedEventLoopGroup!
+    var spanContext: SpanContext!
+    
+    override func setUp() {
+        group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        testServer = NIOHTTP1TestServer(group: group)
+        
+        let spanId = SpanId.random()
+        let traceId = TraceId.random()
+        spanContext = SpanContext.create(traceId: traceId, spanId: spanId, traceFlags: TraceFlags(), traceState: TraceState())
+    }
+    
+    override func tearDown() {
+        XCTAssertNoThrow(try testServer.stop())
+        XCTAssertNoThrow(try group.syncShutdownGracefully())
+    }
+    
+    func testExport() {
+        let testBody = "Hello world " + String(Int.random(in: 1...100))
+        let logRecord = ReadableLogRecord(resource: Resource(),
+                                                     instrumentationScopeInfo: InstrumentationScopeInfo(name: "scope"),
+                                                     timestamp: Date(),
+                                                     observedTimestamp: Date.distantPast,
+            
+                                    spanContext: spanContext,
+                                                     severity: .fatal,
+                                                     body: testBody,
+                                                     attributes: ["event.name":AttributeValue.string("name"), "event.domain": AttributeValue.string("domain")])
+        
+        let endpoint = URL(string: "http://localhost:\(testServer.serverPort)")!
+        let exporter = OtlpHttpLogExporter(endpoint: endpoint)
+        
+        let result = exporter.export(logRecords: [logRecord])
+        XCTAssertEqual(result, ExportResult.success)
+        
+        // FIXME: Use protobuf to verify that we have received the correct Log records
+        //XCTAssertEqual(fakeCollector.receivedLogs, LogRecordAdapter.toProtoResourceRecordLog(logRecordList: [logRecord]))
+        
+        XCTAssertNoThrow(try testServer.receiveHead())
+        XCTAssertNoThrow(try testServer.receiveBodyAndVerify() { body in
+            var contentsBuffer = ByteBuffer(buffer: body)
+            let contents = contentsBuffer.readString(length: contentsBuffer.readableBytes)!
+            XCTAssertTrue(contents.contains(testBody))
+        })
+        
+        XCTAssertNoThrow(try testServer.receiveEnd())
+        
+        exporter.shutdown()
+    }
+}
