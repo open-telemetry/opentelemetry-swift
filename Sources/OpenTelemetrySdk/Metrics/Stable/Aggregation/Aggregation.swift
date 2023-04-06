ff3c2a77ce8e4815ec4b6abb1329d0972b1bbea6
Bryce Buchanan <bryce.buchanan@elastic.co>
2023-04-06 14:53:22 -0700

    public methods

--- Sources/OpenTelemetrySdk/Metrics/Stable/Aggregation/Aggregation.swift
+++ Sources/OpenTelemetrySdk/Metrics/Stable/Aggregation/Aggregation.swift
@@ -8,27 +8,27 @@ import Foundation
 public protocol Aggregation : AggregatorFactory {}
 
 public class Aggregations {
-    static func drop() -> Aggregation {
+    public static func drop() -> Aggregation {
         DropAggregation.instance
     }
     
-    static func defaultAggregation() -> Aggregation {
+    public static func defaultAggregation() -> Aggregation {
         DefaultAggregation.instance
     }
     
-    static func sum() -> Aggregation {
+    public static func sum() -> Aggregation {
         SumAggregation.instance
     }
     
-    static func lastValue() -> Aggregation {
+    public static func lastValue() -> Aggregation {
         LastValueAggregation.instance
     }
     
-    static func explicitBucketHistogram() -> Aggregation {
+    public static func explicitBucketHistogram() -> Aggregation {
         ExplicitBucketHistogramAggregation.instance
     }
     
-    static func explicitBucketHistogram(buckets: [Double]) -> Aggregation {
+    public static func explicitBucketHistogram(buckets: [Double]) -> Aggregation {
         ExplicitBucketHistogramAggregation(bucketBoundries:  buckets)
     }
     
