196eec9b8b5312ad4f07ab60cf73b51e182d7570
Kevin Earls <kevinearls@mac.com>
2023-04-10 13:54:23 +0200

    Drop leading 'v' from OTEL version
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- Sources/Exporters/OpenTelemetryProtocol/common/Headers.swift
+++ Sources/Exporters/OpenTelemetryProtocol/common/Headers.swift
@@ -10,7 +10,10 @@ public struct Headers {
     // GetUserAgentHeader returns an OTLP header value of the form "OTel OTLP Exporter Swift/{{ .Version }}"
     // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#user-agent
     public static func getUserAgentHeader() -> String {
-        let version = OpenTelemetry.version
+        var version = OpenTelemetry.version
+        if !version.isEmpty && version.hasPrefix("v") {
+            version = String(version.dropFirst(1))
+        }
         let userAgent = "OTel-OTLP-Exporter-Swift/\(version)"
 
         return userAgent
