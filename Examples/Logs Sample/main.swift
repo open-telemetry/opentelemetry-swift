8611b04672e899c10975b8da26c838118300110e
Marc Schultz <lunij@users.noreply.github.com>
2023-06-16 19:54:25 +0200

    Clean up "Logs Sample"

--- Examples/Logs Sample/main.swift
+++ Examples/Logs Sample/main.swift
@@ -1,7 +1,7 @@
 //
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-// 
+//
 
 import Foundation
 import OpenTelemetryApi
@@ -11,22 +11,26 @@ import Logging
 import GRPC
 import NIO
 
-
 func configure() {
-  let configuration = ClientConnection.Configuration.default(
-      target: .hostAndPort("localhost", 4317),
-      eventLoopGroup: MultiThreadedEventLoopGroup(numberOfThreads: 1))
+    let configuration = ClientConnection.Configuration.default(
+        target: .hostAndPort("localhost", 4317),
+        eventLoopGroup: MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    )
 
-      
-  _ = OtlpLogExporter(channel: ClientConnection(configuration: configuration))
-  
-      OpenTelemetry.registerLoggerProvider(loggerProvider: LoggerProviderBuilder().with(processors: [
-        BatchLogRecordProcessor(logRecordExporter:OtlpLogExporter(channel: ClientConnection(configuration: configuration)))]).build())
-      
-  }
+    let loggerProvider = LoggerProviderBuilder()
+        .with(processors: [
+            BatchLogRecordProcessor(logRecordExporter: OtlpLogExporter(channel: ClientConnection(configuration: configuration)))
+        ])
+        .build()
 
-  configure()
-        
-  let eventProvider = OpenTelemetry.instance.loggerProvider.loggerBuilder(instrumentationScopeName: "myScope").setEventDomain("device").build()
-      
-      
+    OpenTelemetry.registerLoggerProvider(loggerProvider: loggerProvider)
+}
+
+configure()
+
+let eventProvider = OpenTelemetry
+    .instance
+    .loggerProvider
+    .loggerBuilder(instrumentationScopeName: "myScope")
+    .setEventDomain("device")
+    .build()
