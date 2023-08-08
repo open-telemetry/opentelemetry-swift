869dc5acd61860a0ee150fa37e34e3a452ccf4b8
Bryce Buchanan <75274611+bryce-b@users.noreply.github.com>
2023-08-08 08:46:09 -0700

    added protobuf version as exporter user-agent version (#444)

--- Sources/Exporters/OpenTelemetryProtocolCommon/common/Headers.swift
+++ Sources/Exporters/OpenTelemetryProtocolCommon/common/Headers.swift
@@ -7,15 +7,15 @@ import Foundation
 import OpenTelemetryApi
 
 public struct Headers {
-    // GetUserAgentHeader returns an OTLP header value of the form "OTel OTLP Exporter Swift/{{ .Version }}"
-    // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#user-agent
-    public static func getUserAgentHeader() -> String {
-        var version = OpenTelemetry.version
-        if !version.isEmpty && version.hasPrefix("v") {
-            version = String(version.dropFirst(1))
-        }
-        let userAgent = "OTel-OTLP-Exporter-Swift/\(version)"
-
-        return userAgent
+  // GetUserAgentHeader returns an OTLP header value of the form "OTel OTLP Exporter Swift/{{ .Version }}"
+  // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#user-agent
+  public static func getUserAgentHeader() -> String {
+    var version = Constants.OTLP.version
+    if !version.isEmpty && version.hasPrefix("v") {
+      version = String(version.dropFirst(1))
     }
+    let userAgent = "OTel-OTLP-Exporter-Swift/\(version)"
+    
+    return userAgent
+  }
 }
