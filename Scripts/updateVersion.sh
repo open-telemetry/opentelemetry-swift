bf59685d920d20f54d95be3426fcd90528c75c67
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2024-01-18 19:17:58 +0100

    Update updateVersion.sh
    Properly add the new file to the commit

--- Scripts/updateVersion.sh
+++ Scripts/updateVersion.sh
@@ -5,6 +5,8 @@
 
 #Update version number
 sed -E -i '' 's/public static let OTEL_SWIFT_SDK_VERSION = ".+"/public static let OTEL_SWIFT_SDK_VERSION = "'$2\"/ ./Sources/OpenTelemetrySdk/Version.swift
+cat ./Sources/OpenTelemetrySdk/Version.swift
+git add ./Sources/OpenTelemetrySdk/Version.swift
 git commit -m "Updated version number to $2"
 git tag -f $2
 git push -f --tags origin HEAD:main
