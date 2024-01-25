6c963f70fb3ca0ae50b8c8d5e1f9ac6136766c15
Ignacio Bonafonte <nachob@mac.com>
2024-01-25 17:42:59 +0100

    Revert "Update updateVersion.sh (#508)"
    This reverts commit be46a97818a74f9227fdac192e9f176b716e7435.

--- Scripts/updateVersion.sh
+++ Scripts/updateVersion.sh
@@ -5,8 +5,6 @@
 
 #Update version number
 sed -E -i '' 's/public static let OTEL_SWIFT_SDK_VERSION = ".+"/public static let OTEL_SWIFT_SDK_VERSION = "'$2\"/ ./Sources/OpenTelemetrySdk/Version.swift
-cat ./Sources/OpenTelemetrySdk/Version.swift
-git add ./Sources/OpenTelemetrySdk/Version.swift
 git commit -m "Updated version number to $2"
 git tag -f $2
 git push -f --tags origin HEAD:main
