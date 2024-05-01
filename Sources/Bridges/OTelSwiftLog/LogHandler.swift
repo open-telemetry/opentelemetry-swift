dc8b54fd02732f373f7e6c33063778740757844f
khushijain21 <khushij393@gmail.com>
2024-05-01 11:13:21 +0530

    attribute conversion

--- Sources/Bridges/OTelSwiftLog/LogHandler.swift
+++ Sources/Bridges/OTelSwiftLog/LogHandler.swift
@@ -1,92 +1,79 @@
 import Foundation
 import OpenTelemetryApi
 import OpenTelemetrySdk
+import Logging
 
 // let the bridgename be the url of the package?
-let bridgename: string = "OTelSwiftLog"
-let version: string = "1.0.0"
+let bridgename: String = "OTelSwiftLog"
+let version: String = "1.0.0"
 
-public struct instrumentationScope{
+public struct InstrumentationScope{
     public var name: String
-    public var version: String
-    // Have the rest of params as optional?
+    public var version: String?
     public var eventDomain: String?
     public var schemaUrl: String?
     public var includeTraceContext: Bool?
     public var attributes: [String: AttributeValue]?
 }
+
 // Define a custom log handler
 struct OTelLogHandler: LogHandler {
     
     // Define the log level for this handler
-    public var logLevel: Logger.Level = .info
-    var scope: instrumentationScope // Property to store instrumentation scope name
-    var loggerProvider : LoggerProvider  // Property to set LoggerProvider
-    var logger: Logger 
+    public var logLevel: Logging.Logger.Level = .info
+    private var scope: InstrumentationScope // Property to store instrumentation scope name
+    private var loggerProvider : LoggerProvider  // Property to set LoggerProvider
+    private var logger: OpenTelemetryApi.Logger 
 
-    // use instrumentationscope
     // should default logger provider be a noop? or the sdk implementation?
-    init(scope: instrumentationScope? = instrumentationScope(name: bridgename, version: version), 
+    public init(scope: InstrumentationScope? = InstrumentationScope(name: bridgename, version: version), 
         loggerProvider: LoggerProvider? = OpenTelemetrySdk.LoggerProviderSdk) {
+
         self.scope = scope
         self.loggerProvider = LoggerProvider
         let loggerBuilder = self.loggerProvider.loggerBuilder(instrumentationScope.name)
-            .configure(with: instrumentationScope)
+            .configure(with: scope)
         self.logger = loggerBuilder.build()
     }
 
-    func log(level: Logger.Level,
-             message: Logger.Message,
-             metadata: Logger.Metadata?,
+    public func log(level: Logging.Logger.Level,
+             message: Logging.Logger.Message,
+             metadata: Logging.Logger.Metadata?,
              source: String,
              file: String,
              function: String,
              line: UInt) {
 
               
-        // TODO convert metadata to otel attribute
-        var  otelattributes: [String:AttributeValue] = 
-                                  ["source": AttributeValue.string(source), 
-                                   "file": AttributeValue.string(file),
-                                   "function": AttributeValue.string(function),
-                                   "line": AttributeValue.string(line),
-                                   ]
-        let metadataAttributes = metadata?.toAttributes() 
-        // let metadataAttributes1 = self.metadata.toAttributes()  
+        // This converts log atrributes to otel attributes
+        var otelattributes: [String: AttributeValue] = [
+            "source": AttributeValue.string(source),
+            "file": AttributeValue.string(file),
+            "function": AttributeValue.string(function),
+            "line": AttributeValue.int(line),
+        ]
 
-        // otelattributes.merge(metadataAttributes?) { (_, new) in new }
-        // otelattributes.merge(metadataAttributes1) { (_, new) in new } 
+        // Convert metadata from the method parameter to AttributeValue and assign it to otelattributes
+        if let metadata = metadata {
+            let methodMetadata = convertMetadata(metadata)
+            otelattributes.merge(methodMetadata) { _, new in new }
+        }
 
-        logger.logRecordBuilder.setSeverity(convertSeverity(level: level))
-        .setSpanContext(OpenTelemetry.instance.contextProvider.activeSpan?.context)
+        // Convert metadata from the struct property to AttributeValue and merge it with otelattributes
+        let structMetadata = convertMetadata(self.metadata)
+        otelattributes.merge(structMetadata) { _, new in new }
+
+        // Build the log record and emit it
+        self.logger.logRecordBuilder.setSeverity(convertSeverity(level: level))
+        .setSpanContext(OpenTelemetry.instance.contextProvider.activeSpan?.context) 
         .setBody(AttributeValue.string(message))
-        .setAttributes(otelattribute)
+        .setAttributes(otelattributes)
         .emit()
     }
-    
 
-    func convertSeverity(level: Logger.level) -> OpenTelemetryApi.Severity{
-        switch level {
-            case .trace:
-                return OpenTelemetryApi.Severity.trace
-            case  .debug:
-                return OpenTelemetryApi.Severity.debug
-            case .info:
-                return OpenTelemetryApi.Severity.info
-            case .notice:
-                return OpenTelemetryApi.Severity.info2
-            case .warning:
-                return OpenTelemetryApi.Severity.warn
-            case .error:
-                return OpenTelemetryApi.Severity.error
-            case .critical:
-                return OpenTelemetryApi.Severity.error2  //should this be fatal instead?
-            }
-    }
-
-    // should we convert metadata to OTel attributes and store them?
-    var metadata: Logger.Metadata = [:]
-    subscript(metadataKey key: String) -> Logger.Metadata.Value? {
+    // part  of LogHandler protocol
+    var metadata: Logging.Logger.Metadata = [:]
+    subscript(metadataKey key: String) -> Logging.Logger.Metadata.Value? {
         get {
             return metadata[key]
         }
@@ -126,28 +113,52 @@ extension LoggerBuilder {
     }
 }
 
-// TODO
-extension Logger.Metadata {
-    func toAttributes() -> [String: AttributeValue] {
-        var attributes: [String: AttributeValue] = [:]
-        for (key, value) in self {
-            attributes[key] = convertToAttributeValue(value)
-        }
-        return attributes
+func convertMetadata(_ metadata: Logging.Logger.Metadata) -> [String: AttributeValue] {
+    var convertedAttributes: [String: AttributeValue] = [:]
+
+    // Iterate over each key-value pair in the metadata dictionary
+    for (key, value) in metadata {
+        // Convert each value to AttributeValue
+        let attributeValue = convertToAttributeValue(value)
+
+        // Store the converted value with its corresponding key in the attributes dictionary
+        convertedAttributes[key] = attributeValue
     }
-    
-    private func convertToAttributeValue(_ value: Logger.Metadata.Value) -> AttributeValue {
-        switch value {
-        case let value as String:
-            return AttributeValue.string(value)
-        case let value as Int:
-            return AttributeValue.int(Int64(value))
-        case let value as Double:
-            return AttributeValue.double(value)
-        // Add more cases for other types as needed
-        default:
-            // If the type is not supported, convert to string representation
-            return AttributeValue.string(String(describing: value))
+
+    return convertedAttributes
+}
+
+// Function to recursively convert nested dictionaries to AttributeValue
+func convertToAttributeValue(_ value: Logging.Logger.Metadata.Value) -> AttributeValue {
+    switch value {
+    case let nestedDictionary as Logging.Logger.Metadata:
+        // If value is a nested dictionary, recursively convert it
+        var nestedAttributes: [String: AttributeValue] = [:]
+        for (nestedKey, nestedValue) in nestedDictionary {
+            nestedAttributes[nestedKey] = convertToAttributeValue(nestedValue)
         }
+        return AttributeValue.set(AttributeSet(labels: nestedAttributes))
+    default:
+        // For non-dictionary values, use AttributeValue initializer directly
+        return AttributeValue(value)
     }
+}    
+
+func convertSeverity(level: Logging.Logger.level) -> OpenTelemetryApi.Severity{
+    switch level {
+        case .trace:
+            return OpenTelemetryApi.Severity.trace
+        case  .debug:
+            return OpenTelemetryApi.Severity.debug
+        case .info:
+            return OpenTelemetryApi.Severity.info
+        case .notice:
+            return OpenTelemetryApi.Severity.info2
+        case .warning:
+            return OpenTelemetryApi.Severity.warn
+        case .error:
+            return OpenTelemetryApi.Severity.error
+        case .critical:
+            return OpenTelemetryApi.Severity.error2  //should this be fatal instead?
+        }
 }
