09e2fb07f83712002cb71ff9cdee63aa045e7e12
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-03-04 12:15:23 -0800

    added swiftformat to precommit script and docs

--- Scripts/hooks/precommit-script.sh
+++ Scripts/hooks/precommit-script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-if ! command -v swiftlint 2>&1 >/dev/null
-then
-
-  echo "install swiftlint \(v0.0.57\) for pre-commit linting."
-  exit 0
-fi
-
-if [ "$(swiftlint --version)" != "0.57.1" ]
-then 
-echo "swiftlint installed with incorrect version (`swiftlint --version`). Please use version 0.57.1 for pre-commit linting."
-exit 0
-fi
 OIFS="$IFS"
 IFS=$'\n'
 FILE_LIST=($(git diff --cached --name-only | grep "\.swift$"))
@@ -21,12 +9,29 @@ if [ -z "${FILE_LIST}" ]; then
   exit 0
 fi
 
-swiftlint lint --config .swiftlint.yml --strict "${FILE_LIST[@]}"
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-  echo -e "Swiftlint failed!\n   Fix issues above or run:\n\tswiftlint lint --fix $(printf "\"%s\" " "${FILE_LIST[@]}")"
-  echo "Skip linting with '--no-verify'."
-  exit 1
-fi
-echo "Swiftlint passed!"
- exit 1
+if [ -e $(command -v swiftlint 2>&1 >/dev/null) ] && [ "$(swiftlint --version)" == "0.57.1" ]
+then
+  swiftlint lint --config .swiftlint.yml --strict "${FILE_LIST[@]}"
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    echo -e "Swiftlint failed!\n   Fix issues above or run:\n\tswiftlint lint --fix $(printf "\"%s\" " "${FILE_LIST[@]}")"
+    echo "Skip linting with '--no-verify'."
+    exit 1
+  fi
+  echo "Swiftlint passed!"
+else 
+  echo "Install SwiftLint at version 0.57.1 to enable pre-commit linting."
+fi 
+
+if [ -e $(command -v swiftformat 2>&1 > /dev/null) ]; then 
+  swiftformat --lint "${FILE_LIST[@]}"
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    echo -e "Swiftformat linting failed.\n Fix the issues above or run:\n\tswiftformat $(printf "\"%s\" " "${FILE_LIST[@]}")"
+    echo "Skip linting with '--no-verify'."
+    exit 1
+  fi
+  echo "SwiftFormat passed!"
+else
+  echo "Install swiftformat to enable pre-commit format linting."
+fi 
