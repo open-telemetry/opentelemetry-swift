6f51d1c46c5ef4c0cf8f72436821b496ecba03d2
Kuixi Song <krayc425@gmail.com>
2025-07-01 13:41:25 -0700

    Add visionOS support

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     .iOS(.v13),
     .tvOS(.v13),
     .watchOS(.v6),
+    .visionOS(.v1),
   ],
   products: [
     .library(name: "OpenTelemetryApi", targets: ["OpenTelemetryApi"]),
