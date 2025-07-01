c6e1a7ecf5d52d104a0ab72d50a72ca716d4f431
Kuixi Song <krayc425@gmail.com>
2025-06-30 22:10:02 -0700

    remove watchOS macro

--- Sources/Instrumentation/SignPostIntegration/OSSignposterIntegration.swift
+++ Sources/Instrumentation/SignPostIntegration/OSSignposterIntegration.swift
@@ -3,42 +3,39 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#if !os(watchOS)
+import Foundation
+import os
+import OpenTelemetryApi
+import OpenTelemetrySdk
 
-  import Foundation
-  import os
-  import OpenTelemetryApi
-  import OpenTelemetrySdk
+/// A span processor that decorates spans with the origin attribute
+@available(iOS 15.0, macOS 12, tvOS 15.0, watchOS 8.0, *)
+public class OSSignposterIntegration: SpanProcessor {
 
-  /// A span processor that decorates spans with the origin attribute
-  @available(iOS 15.0, macOS 12, tvOS 15.0, watchOS 8.0, *)
-  public class OSSignposterIntegration: SpanProcessor {
+  public let isStartRequired = true
+  public let isEndRequired = true
+  public let osSignposter = OSSignposter(subsystem: "OpenTelemetry", category: .pointsOfInterest)
+  public let ossignposterQueue = DispatchQueue(label: "org.opentelemetry.ossignposterIntegration")
+  private var spanIdToStateMap: [String: OSSignpostIntervalState] = [:]
 
-    public let isStartRequired = true
-    public let isEndRequired = true
-    public let osSignposter = OSSignposter(subsystem: "OpenTelemetry", category: .pointsOfInterest)
-    public let ossignposterQueue = DispatchQueue(label: "org.opentelemetry.ossignposterIntegration")
-    private var spanIdToStateMap: [String: OSSignpostIntervalState] = [:]
+  public init() {}
 
-    public init() {}
-
-    public func onStart(parentContext: SpanContext?, span: ReadableSpan) {
-      let state = osSignposter.beginInterval("Span", id: .exclusive, "\(span.name, privacy: .public)")
-      ossignposterQueue.sync {
-        spanIdToStateMap[span.context.spanId.hexString] = state
-      }
+  public func onStart(parentContext: SpanContext?, span: ReadableSpan) {
+    let state = osSignposter.beginInterval("Span", id: .exclusive, "\(span.name, privacy: .public)")
+    ossignposterQueue.sync {
+      spanIdToStateMap[span.context.spanId.hexString] = state
     }
-
-    public func onEnd(span: ReadableSpan) {
-      let state = ossignposterQueue.sync {
-        spanIdToStateMap.removeValue(forKey: span.context.spanId.hexString)
-      }
-      if let state {
-        osSignposter.endInterval("Span", state)
-      }
-    }
-
-    public func forceFlush(timeout: TimeInterval? = nil) {}
-    public func shutdown(explicitTimeout: TimeInterval?) {}
   }
-#endif
+
+  public func onEnd(span: ReadableSpan) {
+    let state = ossignposterQueue.sync {
+      spanIdToStateMap.removeValue(forKey: span.context.spanId.hexString)
+    }
+    if let state {
+      osSignposter.endInterval("Span", state)
+    }
+  }
+
+  public func forceFlush(timeout: TimeInterval? = nil) {}
+  public func shutdown(explicitTimeout: TimeInterval?) {}
+}
