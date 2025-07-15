9adad7ede3119f198ad247b373e1efdda33287c7
Tomer Har Yoffi <tomer.haryoffi@coralogix.com>
2025-07-15 17:00:28 +0300

    fix: crash "Cannot determine the PPI values for current device; returning 0.0f - hopefully this will crash your code (you CANNOT run SVG's that use CM/IN/MM etc until you fix this)"

--- Sources/Instrumentation/URLSession/InstrumentationUtils.swift
+++ Sources/Instrumentation/URLSession/InstrumentationUtils.swift
@@ -5,52 +5,70 @@
 
 import Foundation
 #if os(iOS) || os(tvOS)
-  import UIKit
+import UIKit
 #elseif os(watchOS)
-  import WatchKit
+import WatchKit
 #endif
 
 enum InstrumentationUtils {
-  static func objc_getClassList() -> [AnyClass] {
-    let expectedClassCount = ObjectiveC.objc_getClassList(nil, 0)
-    let allClasses = UnsafeMutablePointer<AnyClass>.allocate(capacity: Int(expectedClassCount))
-    let autoreleasingAllClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(allClasses)
-    let actualClassCount: Int32 = ObjectiveC.objc_getClassList(autoreleasingAllClasses, expectedClassCount)
-
-    var classes = [AnyClass]()
-    for i in 0 ..< actualClassCount {
-      classes.append(allClasses[Int(i)])
-    }
-    allClasses.deallocate()
-    return classes
-  }
-
-  static func instanceRespondsAndImplements(cls: AnyClass, selector: Selector) -> Bool {
-    var implements = false
-    if cls.instancesRespond(to: selector) {
-      var methodCount: UInt32 = 0
-      guard let methodList = class_copyMethodList(cls, &methodCount) else {
-        return implements
-      }
-      defer { free(methodList) }
-      if methodCount > 0 {
-        enumerateCArray(array: methodList, count: methodCount) { _, m in
-          let sel = method_getName(m)
-          if sel == selector {
-            implements = true
-            return
-          }
+    static func objc_getClassList() -> [AnyClass] {
+        let expectedClassCount = ObjectiveC.objc_getClassList(nil, 0)
+        let allClasses = UnsafeMutablePointer<AnyClass>.allocate(capacity: Int(expectedClassCount))
+        let autoreleasingAllClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(allClasses)
+        let actualClassCount: Int32 = ObjectiveC.objc_getClassList(autoreleasingAllClasses, expectedClassCount)
+        
+        var classes = [AnyClass]()
+        for i in 0 ..< actualClassCount {
+            classes.append(allClasses[Int(i)])
         }
-      }
+        allClasses.deallocate()
+        return classes
     }
-    return implements
-  }
-
-  private static func enumerateCArray<T>(array: UnsafePointer<T>, count: UInt32, f: (UInt32, T) -> Void) {
-    var ptr = array
-    for i in 0 ..< count {
-      f(i, ptr.pointee)
-      ptr = ptr.successor()
+    
+    static func objc_getSafeClassList(ignoredPrefixes: [String]? = nil) -> [AnyClass] {
+        let allClasses = objc_getClassList()
+        var safeClasses: [AnyClass] = []
+        
+        for cls in allClasses {
+            let className = NSStringFromClass(cls)
+            if let ignoredPrefixes = ignoredPrefixes {
+                if ignoredPrefixes.contains(where: { className.hasPrefix($0) }) {
+                    continue
+                }
+            }
+            
+            safeClasses.append(cls)
+        }
+        print("Found \(safeClasses.count) safe classes for swizzling")
+        return safeClasses
     }
-  }
+    
+    static func instanceRespondsAndImplements(cls: AnyClass, selector: Selector) -> Bool {
+        var implements = false
+        if cls.instancesRespond(to: selector) {
+            var methodCount: UInt32 = 0
+            guard let methodList = class_copyMethodList(cls, &methodCount) else {
+                return implements
+            }
+            defer { free(methodList) }
+            if methodCount > 0 {
+                enumerateCArray(array: methodList, count: methodCount) { _, m in
+                    let sel = method_getName(m)
+                    if sel == selector {
+                        implements = true
+                        return
+                    }
+                }
+            }
+        }
+        return implements
+    }
+    
+    private static func enumerateCArray<T>(array: UnsafePointer<T>, count: UInt32, f: (UInt32, T) -> Void) {
+        var ptr = array
+        for i in 0 ..< count {
+            f(i, ptr.pointee)
+            ptr = ptr.successor()
+        }
+    }
 }
