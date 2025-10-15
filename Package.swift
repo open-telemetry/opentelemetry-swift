f18617a8de858df5def0c71079d40942d71aa2fd
Bryce Buchanan <75274611+bryce-b@users.noreply.github.com>
2025-10-15 10:17:02 -0700

    Fix formatting of dependencies in Package.swift

--- Package.swift
+++ Package.swift
@@ -32,7 +32,8 @@ let package = Package(
     .package(url: "https://github.com/open-telemetry/opentelemetry-swift-core.git", from: "2.2.0"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.86.0"),
     .package(url: "https://github.com/grpc/grpc-swift.git", exact: "1.26.1"),
-    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.0"),    .package(url: "https://github.com/apple/swift-log.git", from: "1.6.4"),
+    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.0"),    
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.6.4"),
     .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0")
   ],
   targets: [
